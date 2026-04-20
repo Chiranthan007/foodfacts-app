03c0bed06b8a605106dbf433c51505d26b9b908f
Chiranthan007 <chiru777reddy@gmail.com>
2026-04-20 10:30:26 +0530

    feat: display saved items count in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,17 @@
 import { NavLink } from 'react-router-dom'
 
-function NavBar() {
+function NavBar({ savedCount }) {
   return (
-    <nav>
-      <NavLink to="/">Search</NavLink> |{" "}
-      <NavLink to="/saved">Saved</NavLink>
+    <nav className="navbar">
+      <span className="nav-logo">🥗 FoodFacts</span>
+
+      <div className="nav-links">
+        <NavLink to="/">Search</NavLink>
+
+        <NavLink to="/saved">
+          Saved ({savedCount})
+        </NavLink>
+      </div>
     </nav>
   )
 }
