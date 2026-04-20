d7e0708526e85fdbfd0807d297bac664cbb243d9
Chiranthan007 <chiru777reddy@gmail.com>
2026-04-20 10:21:00 +0530

    feat: add saved items reducer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
+import { useReducer } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import HomePage from './pages/HomePage'
 import DetailPage from './pages/DetailPage'
 import SavedPage from './pages/SavedPage'
 
+// reducer
+function savedReducer(state, action) {
+  switch (action.type) {
+    case 'ADD':
+      // avoid duplicates using product code
+      if (state.find((item) => item.code === action.payload.code)) {
+        return state
+      }
+      return [...state, action.payload]
+
+    case 'REMOVE':
+      return state.filter((item) => item.code !== action.payload)
+
+    default:
+      return state
+  }
+}
+
 function App() {
+  const [savedItems, dispatch] = useReducer(savedReducer, [])
+
   return (
     <div>
-      <NavBar />
+      <NavBar savedCount={savedItems.length} />
+
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/product/:barcode" element={<DetailPage />} />
-          <Route path="/saved" element={<SavedPage />} />
+          <Route
+            path="/product/:barcode"
+            element={<DetailPage savedItems={savedItems} dispatch={dispatch} />}
+          />
+          <Route
+            path="/saved"
+            element={<SavedPage savedItems={savedItems} dispatch={dispatch} />}
+          />
         </Routes>
       </main>
     </div>
