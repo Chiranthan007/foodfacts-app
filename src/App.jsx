f044b8a2b421bbc2983c8b222749d928b6de1eb2
Chiranthan007 <chiru777reddy@gmail.com>
2026-04-21 09:20:40 +0530

    refactor: remove useReducer and prop drilling from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
-import { useReducer } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import HomePage from './pages/HomePage'
 import DetailPage from './pages/DetailPage'
 import SavedPage from './pages/SavedPage'
 
-// reducer
-function savedReducer(state, action) {
-  switch (action.type) {
-    case 'ADD':
-      // avoid duplicates using product code
-      if (state.find((item) => item.code === action.payload.code)) {
-        return state
-      }
-      return [...state, action.payload]
-
-    case 'REMOVE':
-      return state.filter((item) => item.code !== action.payload)
-
-    default:
-      return state
-  }
-}
-
 function App() {
-  const [savedItems, dispatch] = useReducer(savedReducer, [])
-
   return (
     <div>
-      <NavBar savedCount={savedItems.length} />
+      <NavBar />
 
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route
-            path="/product/:barcode"
-            element={<DetailPage savedItems={savedItems} dispatch={dispatch} />}
-          />
-          <Route
-            path="/saved"
-            element={<SavedPage savedItems={savedItems} dispatch={dispatch} />}
-          />
+          <Route path="/product/:barcode" element={<DetailPage />} />
+          <Route path="/saved" element={<SavedPage />} />
         </Routes>
       </main>
     </div>
